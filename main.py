eb17464567fde4778a96c9e902a26e20d3618821
asdf <sumiminwooo@gmail.com>
2026-03-22 19:28:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import os
+
+app = FastAPI()
+
+# 현재 디렉터리를 템플릿 경로로 지정
+templates = Jinja2Templates(directory=os.path.dirname(__file__))
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("asdf.html", {"request": request, "name": "Yejun"})from fastapi import FastAPI
 
 app = FastAPI()
 
